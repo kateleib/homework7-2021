190d1c4bfc13560494c166fa5c3df705e739eefc
kateleib <78376789+kateleib@users.noreply.github.com>
2021-11-18 00:41:35 -0500

    Add files via upload

--- video.js
+++ video.js
@@ -1,8 +1,7 @@
-var video = document.querySelector("#player1")
+var video = document.querySelector("#player1");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
-
 });
 
 document.querySelector("#play").addEventListener("click", function() {
@@ -16,22 +15,50 @@ document.querySelector("#pause").addEventListener("click", function() {
 	video.pause();
 });
 
-/* document.querySelector("#slower").addEventListener("click", function() {
-	video.playbackRate -= .05;
-}); */
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log(video.playbackRate);
+	video.playbackRate-=0.05;
+});
 
-/* faster
 document.querySelector("#faster").addEventListener("click", function() {
-	video.playbackRate += .05;
-});*/
+	console.log(video.playbackRate);
+	video.playbackRate+=0.05;
+});
 
-/* document.querySelector('#skip').addEventListener("click", function(){
-*/
+document.querySelector('#skip').addEventListener("click", function(){
+	console.log(video.currentTime);
+	video.currentTime+=15;
 
-/* document.querySelector("#mute").addEventListener("click", function() { */
+	if (video.currentTime>=video.duration){
+		video.currentTime=0;
+		console.log(video.currentTime);
+	}
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("Mute Video");
+
+	if(video.muted){
+		video.muted = false;
+		document.getElementById("mute").innerHTML="Mute"
+	}
+
+	else{
+		video.muted=true;
+		document.getElementById("mute").innerHTML="Unmute"
+	}
+});
 
 document.querySelector("#slider").addEventListener("click", function() {
 	console.log(this.value);
 	video.volume=this.value/100;
 	document.querySelector("#volume").innerHTML=this.value+"%"
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add('oldSchool')
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove('oldSchool')
 });
