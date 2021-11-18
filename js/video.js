a3c5e2d33b00821ddd4fb2c3282152d57bf28991
kateleib <78376789+kateleib@users.noreply.github.com>
2021-11-17 23:52:21 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,11 +1,12 @@
-var video;
+var video = document.querySelector("#player1")
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
-
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play()
+	document.getElementById("volume").innerHTML=video.volume*100+"%";
+	});
